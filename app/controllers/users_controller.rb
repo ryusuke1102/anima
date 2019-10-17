e76096358b0efb21fd48ae45f1aa8e9c2184a2c0
ryu <ryusukeminee@gmail.com>
2019-10-17 21:40:33 +0900

    user-login-improve

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,84 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @user = User.find_by(id: params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(id: params[:id])
+    @user = User.new(
+      name: params[:name], 
+      email: params[:email], 
+      password: params[:password],
+      image_name: "default_users.jpg")
+
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/")
+    else
+      @error_message = "入力内容に誤りがあります"
+      render("users/new")
+
+    end
+  end
+
+    
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def login
+    @user = User.find_by(email: params[:email],password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts")
+    else
+      @error_message = "ユーザーかメールアドレスに誤りがあります"
+      render("/users/login_form")
+    end 
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+ 
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    @post = Post.where(user_id: @user.id)
+    @post.destroy_all
+    flash[:notice] = "ユーザーを削除しました"
+    redirect_to("/")
+
+  end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.password = params[:password]
+
+    if params[:image]
+     @user.image_name = "#{@user.id}.jpg"
+     image = params[:image]
+     File.binwrite("public/users_images/#{@user.image_name}",image.read)
+    end
+
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました。"
+      redirect_to("/users/#{@user.id}/edit")
+    end
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
-  get '/posts',to:'posts#index'
+  post "likes/:post_id/destroy" => "likes#destroy"
+  post "likes/:post_id/create" => "likes#create"
 
+  post "logout" => "users#logout"
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+
+  post "users/create" => "users#create"
+  get "users/:id/edit" => "users#edit"
+  post "users/:id/update" => "users#update"
+  post "users/:id/destroy" => "users#destroy"
+  get 'users/index'
+  get "users/:id" => "users#show"
+  get "signup" => "users#new"
+
+
+  get "login" => "users#login_form"
+  get '/users',to:'users#index'
+  get '/posts',to:'posts#index'
   root 'home#top'
 
+  resources :users
+  resources :posts
 end
