3ec0317980ef4122e409257c7d288bdbba189dd7
ryu <ryusukeminee@gmail.com>
2019-11-24 23:11:08 +0900

    aaa

--- app/models/post.rb
+++
@@ -1,9 +0,0 @@
-class Post < ApplicationRecord
-    validates :content, {presence: true, length: {maximum: 30}}
-    validates :detail, {presence: true, length: {maximum: 350}}
-    validates :user_id, {presence: true}
-def users
-    return User.find_by(id: self.user_id)
-end
-
-end
